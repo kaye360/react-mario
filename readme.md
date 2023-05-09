329f51e6dff589feeb0a9d5761e57840936b0a7c
joshkaye88 <jkaye@shaw.ca>
2023-05-08 20:07:57 -0700

    made small screen notice and update readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+To play this game go here:
+[React Mario](https://react-mario.netlify.app/)
+
+Made by:
+[Josh Kaye](https://joshkaye.dev)
+
+
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,10 +6,15 @@ interface GameProps {
 
 export default function Game({children} : GameProps) {
 	return (
-		<div id="game" className='grid items-center justify-center h-screen'>
-			<div className='relative border border-slate-400 w-[800px] h-[600px]  overflow-hidden'>
-				{children}
+		<>
+			<div id="game" className='hidden md:grid items-center justify-center h-screen'>
+				<div className='relative border border-slate-400 w-[800px] h-[600px]  overflow-hidden'>
+					{children}
+				</div>
 			</div>
-		</div>
+			<div className="md:hidden m-8 p-8 rounded-xl border border-slate-400 bg-slate-100">
+				This game only works on larger screens.
+			</div>
+		</>
 	)
 }
