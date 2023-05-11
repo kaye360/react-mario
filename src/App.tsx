3f754534a6312ef28b683450efa1f6944cef9e58
joshkaye88 <jkaye@shaw.ca>
2023-05-10 22:22:23 -0700

    fix undeclare variable warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useRef, useState } from 'react'
+import { createContext, useEffect, useRef } from 'react'
 import useController from './hooks/useController'
 import useResolveKeyPress from './hooks/useResolveKeyPress'
 import usePlayerPosition from './hooks/usePlayerPosition'
